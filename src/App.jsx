f1f7c17cdafa9a8b3672c3e712e6af919fe1d868
Exigoll <exigoll@bk.ru>
2022-06-14 15:22:35 +0500

    feat: adaptive for pages Skills

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="" element={<Home />} />
           <Route path="skills" element={<Skills />} />
           <Route path="projects" element={<Projects />} />
           <Route path="project/:id" element={<ProjectDisplay />} />
--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -3,32 +3,32 @@ import styles from "./Skills.module.scss";
 function Skills() {
   return (
     <div className={styles.skills}>
-      <h1>Skills</h1>
-      <ol className={styles.list}>
-        <li className={styles.item}>
-          <h2>Front-End</h2>
+      <h1 className={styles.skills__title}>My Skills</h1>
+      <ul className={styles.listSkills}>
+        <li className={styles.listSkills__item}>
+          <h2 className={styles.listSkills__title}>Front-End:</h2>
           <span>
             React, Redux, Redux Saga, Next.js, Formik, React Hook Form,
-            MaterialUI, TailwindCS, CSS3: SCSS / StyledComponents / CSS Modules,
+            MaterialUI, SCSS, TailwindCSS, StyledComponents, StyledModules,
             HTML5
           </span>
         </li>
-        <li className={styles.item}>
-          <h2>Back-End</h2>
+        <li className={styles.listSkills__item}>
+          <h2 className={styles.listSkills__title}>Back-End:</h2>
           <span>Node.js, REST API (Axios / Fetch), MockAPI</span>
         </li>
-        <li className={styles.item}>
-          <h2>Others</h2>
+        <li className={styles.listSkills__item}>
+          <h2 className={styles.listSkills__title}>Others:</h2>
           <span>
             Git, GitHub, Webpack, Gulp, ESLint, Prettier, Husky, BEM, Figma,
             Photoshop
           </span>
         </li>
-        <li className={styles.item}>
-          <h2>Languages</h2>
+        <li className={styles.listSkills__item}>
+          <h2 className={styles.listSkills__title}>Languages:</h2>
           <span>JavaScript, TypeScript</span>
         </li>
-      </ol>
+      </ul>
     </div>
   );
 }
